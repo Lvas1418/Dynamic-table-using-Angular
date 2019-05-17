ef7b37c069d4b52ffc5a5038445107ac3566ce08
Lvas <vas20151418@gmail.com>
2019-05-17 16:44:53 +0300

    table sorting

--- controller/controller.component.ts
+++ controller/controller.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Comment } from '../model/model.module';
+import {Component, OnInit, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Comment} from '../model/model.module';
 
 @Component({
   selector: 'app-controller',
@@ -11,8 +11,10 @@ import { Comment } from '../model/model.module';
 export class ControllerComponent implements OnInit {
 
   defaultComments: Comment[];
+  selectedComment: Comment[];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
 
   ngOnInit() {
@@ -21,8 +23,36 @@ export class ControllerComponent implements OnInit {
   }
 
   getData() {
-    this.http.get("https://jsonplaceholder.typicode.com/comments")
+    this.http.get('https://jsonplaceholder.typicode.com/comments')
       .subscribe((data: Comment[]) => this.defaultComments = data);
   }
 
+  onCommentSelect(comment) {
+    this.selectedComment = comment;
+  }
+
+  onDescendingSort(field) {
+    this.sort(field, 1);
+  }
+
+  onAscendingSort(field) {
+    this.sort(field, 0);
+  }
+
+  sort(field: any, flag: number) {
+    let i;
+    let j;
+    (flag) ? [i, j] = [1, -1] : [i, j] = [-1, 1];
+    this.defaultComments.sort(function (a, b) {
+      console.log(a[field], '  ', b[field]);
+      if (a[field] < b[field]) {
+        return i;
+      }
+      if (a[field] > b[field]) {
+        return j;
+      }
+      return 0;
+    });
+
+  }
 }
--- table/table.component.ts
+++ table/table.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit, Input, Injectable, Output, EventEmitter} from '@angular/core';
-import { Comment } from '../model/model.module';
+import {Component, OnInit, Input, Injectable, Output, EventEmitter} from '@angular/core';
+import {Comment} from '../model/model.module';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
+
 })
 export class TableComponent implements OnInit {
   @Input() comments: Comment[];
-  constructor() { }
+  @Output() commentSelectEvent = new EventEmitter<Comment>();
+  @Output() fieldDescendingSortEvent = new EventEmitter<Comment>();
+  @Output() fieldAscendingSortEvent = new EventEmitter<Comment>();
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
 
+  selectComment(comment: Comment) {
+    //this.selectedComment = comment;
+    this.commentSelectEvent.emit(comment);
+
+    console.log(comment);
+  }
+
+  descendingSorting(field: any) {
+
+    this.fieldDescendingSortEvent.emit(field);
+  }
+
+  ascendingSorting(field: any) {
+
+    this.fieldAscendingSortEvent.emit(field);
+
+  }
 }
